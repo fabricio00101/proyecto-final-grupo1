b25f47f422e1b1524a503105cd3cc49cf89de270
fabricio00101 <jfmilanesio@gmail.com>
2025-11-28 11:26:09 -0300

    Modificación de lógica

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import listar_posts
+from .views import listar_posts, post_detalle
 
 app_name = 'posts'
 
 urlpatterns = [
     # Cuando alguien entre a /posts/ verá la lista
-    path('', listar_posts, name= 'listar_posts'),
+    path('', listar_posts, name = 'listar_posts'),
+    path('detalle/<int:id>/', post_detalle, name = 'post_detalle'),
+    
 ]
 
--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Categoria
 
 # Create your views here.
 
@@ -7,5 +7,35 @@ from .models import Post
 def listar_posts(request):
     # Busca todos los posts
     posts = Post.objects.all()
-    # Renderizamos el html y le enviamos la lista de posts
-    return render(request, 'lista_posts.html', {'posts': posts})
+
+    # Sistema de filtrado por categoría
+    categoria_id = request.GET.get('categoria')
+    if categoria_id:
+        posts = posts.filter(categoria_id=categoria_id)
+
+    # Sistema de ordenamiento
+    orden = request.GET.get('orden')
+    if orden == 'antiguo':
+        posts = posts.order_by('publicado') #Acendente
+    elif orden == 'alfabetico':
+        posts = posts.order_by('titulo') # A-Z
+    elif orden == 'alfabetico_inv':
+        posts = posts.order_by('-titulo') # Z-A
+    else:
+        posts = posts.order_by('-publicado') #Por defecto: Recientes primero
+
+    categorias = Categoria.objects.all()
+
+    contexto = {
+        'posts': posts,
+        'categorias': categorias,
+    }
+    return render(request, 'lista_posts.html', contexto)
+
+
+def post_detalle(request, id):
+    post = get_object_or_404(Post, id=id)
+    contexto={
+        'post': post
+    }
+    return render(request, 'post_detalle.html', contexto)
