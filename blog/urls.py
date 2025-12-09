1f087c67f9f66a6fa0fdb21de567c9bc99c01df2
Lorena <Lorena@gmail.com>
2025-12-09 03:36:05 -0300

    agrego pagina A cerca de

--- blog/urls.py
+++ blog/urls.py
@@ -18,13 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import index
+from .views import index, Acerca_de
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-
+    path('Acerca_de/', Acerca_de, name='Acerca_de'),
     path('publicaciones/', include('apps.posts.urls')),
     
     # Autenticación
--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+def Acerca_de(request):
+    return render(request, 'Acerca_de.html')
