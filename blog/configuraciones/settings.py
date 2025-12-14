9798ce8e39eb42dd0b2f0745522cf84bd7f8ff48
Esteban <nunezesteban750@gmail.com>
2025-12-14 20:11:41 -0300

    Arreglo conflicto de carpetas estáticas

--- blog/configuraciones/settings.py
+++ blog/configuraciones/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR),'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR),'assets'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
